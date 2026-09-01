a992e37c730ef7eeb9812fb078b7c21c3ce6b544
szofficiall <sultaan.khan2002@gmail.com>
2026-09-01 23:32:51 +0500

    some changes and removing some mistakes

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def home_view(request):
-    return HttpResponse("<h1>Home Page</h1>")
+    return HttpResponse("Middleware Example ")
